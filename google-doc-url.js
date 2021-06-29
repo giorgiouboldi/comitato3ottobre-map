7dabd6437343d0ba1142b039a7106152d602340b
giorgiouboldi <giorgio.uboldi@gmail.com>
2021-06-29 15:14:29 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1FoI4isVeMpyD4eA_95GYPD1Kcst4PLgSnvRlClMLgO8/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1FoI4isVeMpyD4eA_95GYPD1Kcst4PLgSnvRlClMLgO8/edit#gid=2017283469';
